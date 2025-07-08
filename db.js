1f8daf988affc69c1a00ac563efe6dafca65d369
unknown <client689k@gmail.com>
2025-07-08 16:50:17 +0700

    update

--- db.js
+++ db.js
@@ -1,36 +1,36 @@
-// import mysql from 'mysql2/promise';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASS,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// export default db;
-
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASS,
-  database: process.env.MYSQLNAME,
-  port: process.env.MYSQLPORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  ssl: {
-    rejectUnauthorized: false
-  }
 });
 
 export default db;
+
+// import mysql from 'mysql2/promise';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const db = mysql.createPool({
+//   host: process.env.MYSQLHOST,
+//   user: process.env.MYSQLUSER,
+//   password: process.env.MYSQLPASS,
+//   database: process.env.MYSQLNAME,
+//   port: process.env.MYSQLPORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// export default db;
